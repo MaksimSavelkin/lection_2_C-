aa47776781ffbac6a4dbd56110d64ecd382ced74
msavelkin <msavelkin@gmail.com>
2022-07-13 21:04:29 +0300

    V2-3

---
+++ L2-3/Program.cs
@@ -0,0 +1,17 @@
+﻿int[] array = {11, 32, 23, 18, 35, 16, 27, 18};
+
+int n = array.Length;
+int find = 18;
+
+int i = 0;
+
+while (i < n)
+{
+
+    if(array[i] == find)
+    {
+        Console.WriteLine(i);
+        break;
+    }
+    i= i + 1;
+}
